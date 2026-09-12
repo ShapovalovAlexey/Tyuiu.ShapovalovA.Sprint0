297467efae18da06f25eaceab67947b2775ccf90
Alexey Shapovalov <Tyuiu.ShapovalovA@outlook.com>
2026-09-12 15:14:10 +0500

    Task3

---
+++ Tyuiu.ShapovalovA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.ShapovalovA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.ShapovalovA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
